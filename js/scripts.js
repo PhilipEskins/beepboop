fa0a9e6dd61d9bbd872ff9178ed61370bfd6b65f
Student <example@example.com>
2019-01-25 13:35:15 -0800

    add option to sort from small to large or large to small

--- js/scripts.js
+++ js/scripts.js
@@ -23,18 +23,53 @@ function beepboop(inputNum, name) {
   return output = numArr;
 }
 
+function boopbeep(inputNum, name) {
+  var numArr = [];
+  var output = "";
+
+  for (var i = inputNum; i > 0; i--) {
+    var string = i.toString();
+    var checkOne = string.indexOf("1");
+    var checkTwo = string.indexOf("2");
+    var checkThree = string.indexOf("3");
+    if (checkThree > -1) {
+      if (!!name) {
+        numArr.push(i + " = I'm sorry, " + name + ". I'm afraid I can't do that.");
+      } else {
+        numArr.push(i + " = I'm sorry, Dave. I'm afraid I can't do that.");
+      }
+    } else if (checkTwo > -1) {
+      numArr.push(i + " = Boop!");
+    } else if (checkOne > -1) {
+      numArr.push(i + " = Beep!");
+    } else {
+    numArr.push(i); }
+  }
+  return output = numArr;
+}
+
 $(document).ready(function(){
   $("form#project3").submit(function(event){
     event.preventDefault();
     var name = $("#name").val()
     var nameUpper = name.charAt(0).toUpperCase() + name.slice(1);
     var number = parseInt($("#number").val());
-    var numArr = beepboop(number, nameUpper);
+    var order = $("input:radio[name=operation]:checked").val();
     var itemList = [];
     $(".result").html("");
-    for (var k = 0; k < numArr.length; k++) {
-      var displayList = "<li>" + numArr[k] + "</li>";
-      $(".result").append($.parseHTML(displayList));
+    if (order === "small") {
+      var numArrSmall = beepboop(number, nameUpper);
+      for (var k = 0; k < numArrSmall.length; k++) {
+        var displayList = "<li>" + numArrSmall[k] + "</li>";
+        $(".result").append($.parseHTML(displayList));
+      }
+    }
+    if (order === "large") {
+      var numArrLarge = boopbeep(number, nameUpper);
+      for (var l = 0; l < numArrLarge.length; l++) {
+        var displayList = "<li>" + numArrLarge[l] + "</li>";
+        $(".result").append($.parseHTML(displayList));
+      }
     }
     // $(".result").text(testItem);
     // $(".result").text(beepboop(number, nameUpper));
